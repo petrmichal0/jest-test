4e899613a8fbbb90073c51f749daec9f0cd82c05
Petr Michal <petrmichal0@gmail.com>
2024-10-24 20:50:05 +0200

    toEquel and object

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,21 @@
 export function toUpperCase(arg: string): string {
   return arg.toUpperCase();
 }
+
+export type stringInfo = {
+  lowerCase: string;
+  upperCase: string;
+  characters: string[];
+  length: number;
+  extraInfo: object | undefined;
+};
+
+export function getStringInfo(input: string): stringInfo {
+  return {
+    lowerCase: input.toLowerCase(),
+    upperCase: input.toUpperCase(),
+    characters: Array.from(input),
+    length: input.length,
+    extraInfo: {},
+  };
+}
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,7 +1,18 @@
 import { toUpperCase } from "../app/Utils";
+import { getStringInfo } from "../app/Utils";
 
 describe("Utils", () => {
   test("converts a string to uppercase", () => {
     expect(toUpperCase("hello")).toBe("HELLO");
+  });
+
+  test.only("returns an info for valid string", () => {
+    expect(getStringInfo("hello")).toEqual({
+      lowerCase: "hello",
+      upperCase: "HELLO",
+      characters: ["h", "e", "l", "l", "o"],
+      length: 5,
+      extraInfo: {},
+    });
   });
 });
